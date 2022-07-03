5d3bf2a6de3500664a32efb2018f05610ce5c68f
Danielle <dziemba88@gmail.com>
2022-07-03 18:56:44 -0400

    Implement add new student feature

--- src/main/java/com/github/danielleziemba/sms/controller/StudentController.java
+++ src/main/java/com/github/danielleziemba/sms/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.github.danielleziemba.sms.controller;
 
+import com.github.danielleziemba.sms.entity.Student;
 import com.github.danielleziemba.sms.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,5 +22,21 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    // handler add new student
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+
+        // TODO: create student object to hold student form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String savestudent(@ModelAttribute("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
